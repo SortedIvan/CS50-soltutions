c72eec40c6893bd042e7d2999b2bf3724a7d6ab3
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-21 13:48:05 +0000

    Wed, Sep 21, 2022, 1:48 PM +00:00

--- hello.c
+++ hello.c
@@ -2,6 +2,6 @@
 #include <cs50.h>
 
 int main(void){
-    string answer = get_string("What my name bro? ");
-    printf($"hello, ");
+    string answer = get_string("What my name bro? \n");
+    printf("hello, %s \n", answer);
 }
