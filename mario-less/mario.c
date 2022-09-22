ddd281a93816d4a39dc40724e919a3a5a9b1f800
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-22 14:24:58 +0000

    Thu, Sep 22, 2022, 2:24 PM +00:00

---
+++ mario-less/mario.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = -1;
+    do{
+        height = get_int("Enter height:" );
+    }
+    while(height < 0);
+
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < height; j++){
+            
+        }
+    }
+
+
+}
