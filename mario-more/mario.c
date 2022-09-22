71bf2e3beaf8007f29d3924edb0abee9f6013bb2
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-22 18:54:24 +0000

    Thu, Sep 22, 2022, 6:54 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,73 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+void marioRight(void);
+void marioLeft(void);
+void marioHard(void);
+
+
+int main(void)
 {
-    
+    marioHard();
+}
+
+void marioRight(void)
+{
+    int height = -1;
+    do
+    {
+        height = get_int("Enter height:" );
+    }
+    while(height < 0);
+
+    for (int i = 0; i < height; i++)
+    {
+        int count = i;
+
+        while (count >= 0)
+        {
+            printf("#");
+            count--;
+        }
+        printf("\n");
+    }
+}
+// comment
+void marioLeft(void)
+{
+    int height = -1;
+    do
+    {
+        height = get_int("Enter height: ");
+    }
+    while (height < 0);
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int k = height - i; k > 0; k--){
+            printf(".");
+        }
+        int j = 0;
+        while (j < i){
+            printf("#");
+            j++;
+        }
+        printf("\n");
+    }
+
+}
+
+void marioHard(void)
+{
+    int height = -1;
+    do
+    {
+        height = get_int("Enter height: ");
+    }
+    while (height < 0);
+
+    for(int row = 0; row < height; row++)
+    {
+        // right side of mario
+    }
 }
