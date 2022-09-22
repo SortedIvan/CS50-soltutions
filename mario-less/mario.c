e70862662156c2f427639b37cf2bd9474c6a9133
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-22 15:22:10 +0000

    Thu, Sep 22, 2022, 3:22 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -40,8 +40,15 @@ void marioLeft(void)
     }
     while (height < 0);
 
-    for (int i = height; i > 0; i--)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 1; j < h)
+        for (int j = height; j > 0; j--){
+            printf(".");
+        }
+        for (int k = 0; k < i; k++){
+            printf("#");
+        }
+        printf("\n");
     }
+
 }
