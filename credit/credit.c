8984e239a5a7d7f660d5d8af34b812cabca6e2df
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-23 11:59:31 +0000

    Fri, Sep 23, 2022, 11:59 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool validateCreditCard();
+
 int main(void)
 {
 
@@ -9,11 +11,14 @@ int main(void)
 bool validateCreditCard()
 {
     long credit = get_long("Enter your credit card nr: ");
+    sum = 0;
+    while (credit > 0) {
+        int digit = value % 10;
+        digit = digit * 2;
 
-    int value = 123;
-    while (value > 0) {
-    int digit = value % 10;
-    value /= 10;
+        if (digit )
+
+        value /= 10;
     }
 
 }
