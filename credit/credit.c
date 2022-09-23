4755d8a5d4458b196959a56275dbd69733bf50f4
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-23 12:07:58 +0000

    Fri, Sep 23, 2022, 12:07 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,19 +5,27 @@ bool validateCreditCard();
 
 int main(void)
 {
-
+    validateCreditCard();
 }
 
 bool validateCreditCard()
 {
     long credit = get_long("Enter your credit card nr: ");
-    sum = 0;
-    
+    int sum = 0;
+
     credit /= 10;               // throw away the least significant digit
-    for (int i = 0; i < 7; ++i)  // for each odd digit
+    while (credit > 0)  // for each odd digit
     {
-        digits[i] = cardNum %10; // extract the digit
-        cardNum /= 100;          // throw away two digits
+        int digit = credit % 10;
+
+        if (digit > 9){
+            digit = digit - 9;
+        }
+
+        sum += digit;
+        credit /= 100;          // throw away two digits
     }
+    printf("%i", sum);
+    return true;
 
 }
