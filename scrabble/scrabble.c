d90b65718f271e78dfd4c37726444f1e581fde12
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-24 16:20:34 +0000

    Sat, Sep 24, 2022, 4:20 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,6 +2,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
@@ -28,7 +29,7 @@ int compute_score(string word)
         i++;
     }
 
-    for (int j = 0; j < i; j++){
-        
+    for(int j = 0; str[j]; j++){
+    word[j] = tolower(str[i]);
     }
 }
