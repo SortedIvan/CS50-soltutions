1fa1b04be953c9ec8c7154d418f1b612ceaa83ec
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-24 16:47:15 +0000

    Sat, Sep 24, 2022, 4:47 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -7,6 +7,7 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
+bool determine_winner(int score1, int score2);
 
 int main(void)
 {
@@ -17,11 +18,23 @@ int main(void)
     // Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
+    determine_winner(score1,score2);
 
-    printf("%i", score1);
-    printf("%i", score2);
 
-    // TODO: Print the winner
+}
+
+bool determine_winner(int score1, int score2){
+    if (score1 > score2){
+        printf("Player one wins with: %i score\n", score1);
+        return true;
+    }
+    else if (score1 < score2){
+        printf("Player two wins with: %i score\n", score2);
+        return true;
+    }
+
+    printf("TIE!\n");
+    return false;
 }
 
 int compute_score(string word)
@@ -33,22 +46,23 @@ int compute_score(string word)
     while (word[word_length] != '\0'){
         word_length++;
     }
+    // make all letters capital for ASCII
+    for (int i = 0; i < word_length; i++){
+        if(islower(word[i])){
+            word[i] = toupper(word[i]);
+        }
+    }
 
     for (int i = 0; i < word_length; i++){
         // Checks whether there are any special characters and ignores them
         if((int)word[i] < 64 || (int)word[i] > 90){
             continue;
         }
-
-        if(islower(word[i])){
-            word[i] = toupper(word[i]);
-        }
-
-        // 'D' - 'A' = D points 68 - 64 4 ( minus 1)
-
         score += POINTS[(int)word[i] - 65];
-
     }
+
+
+
     return score;
     // 64 - A
     // 90 - Z
