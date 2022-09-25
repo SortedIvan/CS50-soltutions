08da81c939ac088b4ee183b0daaeb2741d3749d6
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-25 17:37:24 +0000

    Sun, Sep 25, 2022, 5:37 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,11 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int TestFormula(string input, int key);
+
 int main(int argc, string argv[])
 {
-
+    printf(TestFormula(argv[0], argc);
 }
 
-void cypherText(){
-    
+int TestFormula(string input, int key){
+
+    int encryptedASCII = 0;
+    encryptedASCII = ((int)input[0] + key) % 26;
+    return encryptedASCII;
 }
