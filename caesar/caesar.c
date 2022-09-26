3e013fecfc734c8d2d0b26d388d2bfc6defcc128
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-26 17:54:50 +0000

    Mon, Sep 26, 2022, 5:54 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,12 +8,5 @@ int TestFormula(string input, int key);
 
 int main(int argc, string argv[])
 {
-    printf("ye: %i\n", TestFormula(argv[0], argc));
-}
 
-int TestFormula(string input, int key){
-
-    int encryptedASCII = 0;
-    encryptedASCII = ((int)input[0] + key) % 26;
-    return encryptedASCII;
 }
