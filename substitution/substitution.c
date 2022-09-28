fe0317a26d4933a38a5a4a1fa28a0e4447e9427b
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-28 11:27:01 +0000

    Wed, Sep 28, 2022, 11:27 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    if(argc != 2)
+    {
+        return 1;
+        printf("Usage: ./substitution key");
+    }
+
+    string key = argv[1]; // sequence of how the alphabet is arranged
+    string message = get_string("Provide a word to encrypt: ");
+
+
+    
 
 }
