58be26455621104eec42448561634b6cf5f927c2
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-28 11:40:17 +0000

    Wed, Sep 28, 2022, 11:40 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -14,8 +14,15 @@ int main(int argc, string argv[])
 
     string key = argv[1]; // sequence of how the alphabet is arranged
     string message = get_string("Provide a word to encrypt: ");
+    string ciphered_message = encrypt_message(message, key);
 
 
+    printf("Ciphered text: ");
+    for (int i = 0; i < get_string_len(ciphered_message); i++)
+    {
+        printf("%c", message[i]);
+    }
+    printf("\n");
 
     // A - 65
     // Z - 90
@@ -25,14 +32,12 @@ string encrypt_message(string message, string key)
 {
     for(int i = 0; i < get_string_len(message); i++)
     {
-        for(int k = 0; k < get_string_len(key); k++)
-        {
-            
-
+        int alphabetic_pos = (int)message[i] - (int)'A';
+        message[i] = key[alphabetic_pos];
         //NQXPOMAFTRHLZGECYJIUWSKDVB - key[i] = N
         // takes message[i] where it would usually sit in a normal alph order = key[i]
-        }
     }
+    return message;
 }
 
 int get_string_len(string string_message)
