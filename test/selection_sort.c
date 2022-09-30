c1dcd205cc2d9ec5b8ea0c82e57d8639edcae893
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-30 17:03:05 +0000

    Fri, Sep 30, 2022, 5:03 PM +00:00

--- test/selection_sort.c
+++ test/selection_sort.c
@@ -1,13 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_array_size(int some_array[]);
 void sort_array(int unsorted_array[]);
 
 int main()
 {
-    int random_array[] = {1,3,4,5};
-    printf("%i", get_array_size(random_array));
 }
 
 void sort_array(int unsorted_array[])
@@ -17,10 +14,5 @@ void sort_array(int unsorted_array[])
 
 int get_array_size(int some_array[])
 {
-    int size = 0;
-    while(some_array[size] != NULL)
-    {
-        size++;
-    }
-    return size;
+
 }
