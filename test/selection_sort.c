3687db76acfb9f729502214628e4035fb267521e
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-30 17:13:55 +0000

    Fri, Sep 30, 2022, 5:13 PM +00:00

--- test/selection_sort.c
+++ test/selection_sort.c
@@ -1,13 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void sort_array(int unsorted_array[]);
+void sort_array(int unsorted_array[], int length);
 
 int main()
 {
+    int length = get_int("Provide array size:" );
+
 }
 
-void sort_array(int unsorted_array[])
+void sort_array(int unsorted_array[], int length)
 {
-    int size = sizeof unsorted_array / sizeof unsorted_array[0];
+    int smallest_nr = unsorted_array[0];
+    int sorted_array[length];
+    int counter = 0;
+
+    while (counter <= length)
+    {
+        for(int i = 0; i < length; i++)
+        {
+            if (sorted_array[i] < smallest_nr)
+            {
+                smallest_nr = sorted_array[i];
+            }
+        }
+    }
 }
