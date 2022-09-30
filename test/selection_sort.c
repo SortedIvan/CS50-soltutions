0c9b0ac1fd82370bef72a8106a42f9aa23cf3424
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-09-30 17:21:13 +0000

    Fri, Sep 30, 2022, 5:21 PM +00:00

--- test/selection_sort.c
+++ test/selection_sort.c
@@ -1,14 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
+#define randnum(min, max) \ ((rand() % (int)(((max) + 1) - (min))) + (min))
 void sort_array(int unsorted_array[], int length);
 
 int main()
 {
-
+    srand(time(NULL));
     int length = get_int("Provide array size:" );
     int unsorted_array[length];
-    sort_array(int unsorted_array[], int length)
+
+    // Populating array
+    for (int i = 0; i < length; i++)
+    {
+        unsorted_array[i] = randnum(1,50);
+    }
+
+    // Printing unsorted array
+    for (int i = 0; i < length; i++)
+    {
+        printf("%i", unsorted_array[i]);
+    }
+    printf("\n");
+
 
 }
 
@@ -17,6 +33,9 @@ void sort_array(int unsorted_array[], int length)
     int smallest_nr = unsorted_array[0];
     int sorted_array[length];
     int counter = 0;
+
+
+
 
     while (counter <= length)
     {
