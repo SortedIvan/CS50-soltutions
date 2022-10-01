025e2d39ff524eb429904ee903743df6abf49c29
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-10-01 10:45:58 +0000

    Sat, Oct 1, 2022, 10:45 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -83,6 +83,7 @@ bool vote(string name)
 void print_winner(void)
 {
     int biggest_vote = candidates[0].votes;
+    
     for (int i = 0; i < candidate_count; i++)
     {
         if(candidates[i].votes > biggest_vote)
