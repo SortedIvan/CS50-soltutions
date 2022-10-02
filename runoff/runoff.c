c110e9c7cbc6b26f4dee0d1426696ca209ba780a
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-10-02 15:59:39 +0000

    Sun, Oct 2, 2022, 3:59 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     // Check whether the person {name} exists
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].name == name)
+        if (strcmp(candidates[i].name,name) == 0)
         {
             preferences[voter][rank] = i;
             return true;
