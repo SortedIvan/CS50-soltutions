608b49e857d0e5b658dd8737493e3793eedf4275
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-10-02 15:56:45 +0000

    Sun, Oct 2, 2022, 3:56 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,13 +127,13 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // voter -> 
+    // voter ->
     // Check whether the person {name} exists
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].name == name)
         {
-
+            
         }
     }
     return false;
