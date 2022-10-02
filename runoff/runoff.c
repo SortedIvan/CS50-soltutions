ce53a1516908efbcb66cc02c7485009e9bd65406
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-10-02 23:17:36 +0000

    Sun, Oct 2, 2022, 11:17 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -214,13 +214,19 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
+    int biggest_vote = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         if (!candidates[i].eliminated)
         {
-            
+            if (candidates[i].votes > biggest_vote)
+            {
+                biggest_vote = candidates[i].votes;
+            }
         }
     }
+
+
 
 
     return false;
