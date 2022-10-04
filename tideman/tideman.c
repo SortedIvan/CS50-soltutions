3be610fa46165e061bb326682a571b63766090d8
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-10-04 15:20:24 +0000

    Tue, Oct 4, 2022, 3:20 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -121,7 +121,15 @@ void record_preferences(int ranks[])
 {
     // TODO
     // ranks[i] = ith preference (Alice, Bob, Charlie, aka if ranks[0] = 2, that means that Charlie is the first preference)
-    
+
+    for(int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (i != j)
+        }
+    }
+
     return;
 }
 
