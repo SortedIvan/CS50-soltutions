e3efa03f134fc8a8554f3c3fd54d40a13690b584
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-10-04 17:38:21 +0000

    Tue, Oct 4, 2022, 5:38 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -123,7 +123,7 @@ void record_preferences(int ranks[])
             if (p1 != p2)
             {
                 preferences[ranks[p1]][ranks[p2]]++;
-                printf(" %i ", preferences[ranks[p1]][ranks[p2]]);
+                printf("- %i %i -", ranks[p1], ranks[p2]);
             }
         }
     }
