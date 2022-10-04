e4b54f9bf8b8d6c76fcc379a01d8120d69b97d52
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-10-04 14:52:55 +0000

    Tue, Oct 4, 2022, 2:52 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -99,7 +99,14 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i], name) == 0)
+        {
+            // valid name - candidate exists
+            
+        }
+    }
     return false;
 }
 
