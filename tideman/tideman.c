dd0f01a33ab6b5aa349414b0433051a061d5265f
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2022-10-05 16:42:38 +0000

    Wed, Oct 5, 2022, 4:42 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -90,7 +90,7 @@ int main(int argc, string argv[])
         printf("\n");
     }
 
-    //add_pairs();
+    add_pairs();
     sort_pairs();
     lock_pairs();
     print_winner();
@@ -143,32 +143,25 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    pair_count = 0;
-    for (int i = 0; i < candidate_count; i++)
+    for(int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for(int j = i + 1; j < candidate_count; j++)
         {
-            if (preferences[i][j] > preferences[j][i])
+            if(preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
-                pair_count++;
+                pair_count ++;
             }
-
-            if (preferences[i][j] < preferences[j][i])
+            else if(preferences[i][j] < preferences[j][i])
             {
                 pairs[pair_count].winner = j;
                 pairs[pair_count].loser = i;
-                pair_count++;
+                pair_count ++;
             }
         }
     }
-
-    // for (int i = 0; i < pair_count; i++)
-    // {
-    //     printf("pair at: %i is %i over %i \n", i, pairs[i].winner, pairs[i].loser);
-    // }
-
+    // TODO
     return;
 }
 
